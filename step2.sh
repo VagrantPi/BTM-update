2b12f059d8a3799f5fcab5439015c19174ad67a3
VagrantPi <xup6u06m4@gmail.com>
2025-01-06 17:40:42 +0800

    add dependency install

--- step2.sh
+++ step2.sh
@@ -47,7 +47,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 # install
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin unzip docker-compose -y
 
 
 # Step 4. clone project
