62a9c70b91dd5a821107cfa46e337234c2c19cd9
VagrantPi <xup6u06m4@gmail.com>
2025-03-03 10:00:21 +0800

    feat: update README

--- step2-1.sh
+++ step2-1.sh
@@ -23,21 +23,6 @@ echo "" >> /usr/lib/node_modules/lamassu-server/.env
 echo "BTM_API_URL=http://127.0.0.1:8080" >> /usr/lib/node_modules/lamassu-server/.env
 echo "BTM_API_KEY=$SERVER_KEY" >> /usr/lib/node_modules/lamassu-server/.env
 
-
-# pull lamassu-server code
-rm /usr/lib/node_modules/lamassu-server/lib/whitelist.js
-curl -L https://raw.githubusercontent.com/VagrantPi/lamassu-server-release/refs/heads/main/lib/btm-api.js \
-     -o /usr/lib/node_modules/lamassu-server/lib/btm-api.js
-curl -L https://raw.githubusercontent.com/VagrantPi/lamassu-server-release/refs/heads/main/lib/btm-cibs.js \
-     -o /usr/lib/node_modules/lamassu-server/lib/btm-cibs.js
-curl -L https://raw.githubusercontent.com/VagrantPi/lamassu-server-release/refs/heads/main/lib/cash-in/cash-in-tx.js \
-     -o /usr/lib/node_modules/lamassu-server/lib/cash-in/cash-in-tx.js
-curl -L https://raw.githubusercontent.com/VagrantPi/lamassu-server-release/refs/heads/main/lib/db.js \
-     -o /usr/lib/node_modules/lamassu-server/lib/db.js
-curl -L https://raw.githubusercontent.com/VagrantPi/lamassu-server-release/refs/heads/main/lib/routes/customerRoutes.js \
-     -o /usr/lib/node_modules/lamassu-server/lib/routes/customerRoutes.js
-
-
 # 重啟後台
 cd BTM-backend
 git pull
@@ -45,6 +30,11 @@ docker-compose build
 docker-compose down
 docker-compose up -d
 
+# pull lamassu-server code
+cd /usr/lib/node_modules/lamassu-server
+git pull
 
 # 重啟 lamassu-server
 supervisorctl restart lamassu-admin-server lamassu-server
+
+cd ~
